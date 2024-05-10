163cdd6cc43f2d344e7060e0de8d81c89bef0217
andreslqr <me@andreslopez.com.mx>
2024-05-10 13:21:16 -0600

    added first test

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_user_can_create_a_project(): void
+    {
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph()
+        ];
+
+        $response = $this->post('/projects', $attributes);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+}
