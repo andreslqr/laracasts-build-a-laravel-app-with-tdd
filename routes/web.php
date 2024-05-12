2fc3a89a89ccf0e95b84270c4690ee33b63fb5e0
andreslqr <me@andreslopez.com.mx>
2024-05-11 19:31:52 -0600

    feat: view for list projects

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/projects', function() {
+    $projects = Project::all();
+
+    return view('projects.index', [
+        'projects' => $projects
+    ]);
+});
+
 Route::post('/projects', function(Request $request) {
     Project::create($request->only('title', 'description'));
 });
