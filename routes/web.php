230292d659f2a8eed699a7848cdb46f182e8f4ed
andreslqr <me@andreslopez.com.mx>
2024-05-11 19:36:58 -0600

    refactor: changed to controllers

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,6 @@
 <?php
 
-use App\Models\Project;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/projects', function() {
-    $projects = Project::all();
-
-    return view('projects.index', [
-        'projects' => $projects
-    ]);
-});
-
-Route::post('/projects', function(Request $request) {
-    Project::create($request->only('title', 'description'));
-});
+Route::resource('projects', ProjectsController::class);
