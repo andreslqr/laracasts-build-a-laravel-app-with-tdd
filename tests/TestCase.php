76ef4e41cb1f7db5918a1382e8e7c62a0ca9d463
andreslqr <me@andreslopez.com.mx>
2024-05-14 17:48:16 +0000

    fix: removed vite from tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
