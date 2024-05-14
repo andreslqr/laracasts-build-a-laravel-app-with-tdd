9d321941235f67d9c3ab879fe6de2dda3ed8ac2c
andreslqr <me@andreslopez.com.mx>
2024-05-14 17:59:38 +0000

    test: relationship between projects and users

--- tests/Unit/Models/PojectTest.php
+++ tests/Unit/Models/PojectTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
@@ -17,5 +18,13 @@ class PojectTest extends TestCase
         $project = Project::factory()->create();
 
         $this->assertStringEndsWith("/projects/{$project->getKey()}", $project->path());
+    }
+
+    #[Test]
+    public function it_belongs_to_an_owner(): void
+    {
+        $project = Project::factory()->create();
+
+        $this->assertInstanceOf(User::class, $project->owner);
     }
 }
