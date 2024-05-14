169b2f53ca23a63d568a0d9f05e82ada1c5cc7a7
andreslqr <me@andreslopez.com.mx>
2024-05-14 17:22:26 +0000

    added relationship with projects

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -4,11 +4,14 @@ namespace Tests\Unit\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+    
     #[Test]
     public function a_user_has_projects()
     {
