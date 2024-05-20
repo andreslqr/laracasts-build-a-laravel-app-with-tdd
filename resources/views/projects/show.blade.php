d39c2142056e571dbe1838adcbe7d8985154fcd5
andreslqr <me@andreslopez.com.mx>
2024-05-20 10:34:47 -0600

    feat: added layouts for projects

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,11 +7,28 @@
     <title>Birdboard - {{ $project->title }}</title>
 </head>
 <body>
-    <h1>
-        {{ $project->title }}
-    </h1>
-    <p>
-        {{ $project->description }}
-    </p>
+    
 </body>
 </html>
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Projects') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <h1>
+                    {{ $project->title }}
+                </h1>
+                <p>
+                    {{ $project->description }}
+                </p>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
